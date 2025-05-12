79ae21417efc1a40cb1ec4e32748104ec4f0729d
Michael Chow <mc_al_github@fastmail.com>
2025-05-12 17:01:06 -0400

    docs: remove homepage files

--- homepage/features/code/facet.py
+++
@@ -1,5 +0,0 @@
-!!!(
-    ggplot(anscombes_quartet, aes("x", "y", color="dataset"))
-    + geom_point()
-    ???+ facet_wrap("dataset")!!!
-)???
--- homepage/features/code/layer.py
+++
@@ -1,6 +0,0 @@
-!!!(
-    ggplot(anscombes_quartet, aes("x", "y", color="dataset"))
-    + geom_point()
-    ???+ geom_smooth(method="lm", se=False, fullrange=True)!!!
-    + facet_wrap("dataset")
-)???
